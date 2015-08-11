58ee11781b477575c3ab11a7662642761ca6ded5
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-11 10:46:12 +0100

    Added some bookmarks

---
+++ app/app.rb
@@ -0,0 +1,29 @@
+require 'sinatra/base'
+require './app/models/link'
+
+
+class BookmarkManager < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views') }
+
+  get '/' do
+    "Hello Bookmark Manager"
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/form'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
+  end
+
+
+
+end
