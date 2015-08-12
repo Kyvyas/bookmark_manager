62685cacdff7c04f61d07a73959867c8a07d6d5b
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-12 10:49:23 +0100

    Added users and passwords

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,9 @@ class BookmarkManager < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     "Hello Bookmark Manager"
   end
@@ -46,6 +49,18 @@ class BookmarkManager < Sinatra::Base
     redirect to('/links')
   end
 
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      User.get(session[:user_id])
+    end
+  end
 
 
 end
